25c67a9cfdc3e4d6141e17c7ef81168b09f71d1a
Santiago Fuentes <santiago@fuentes.it.com>
2026-06-27 12:53:43 -0600

    added proper path

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   reactStrictMode: true,
   experimental: {
     optimizePackageImports: ["@mantine/core", "@mantine/hooks"],
   },
-  output: "export",
   images: {
     unoptimized: true,
   },
+  generateBuildId: async () => "build",
+  // disable prefetch txt files
+  pageExtensions: ["tsx", "ts", "jsx", "js"],
 };
-
 export default nextConfig;
